997df224b2a7c770da3ba380e332389f340def75
Anastasia Karpova <vip.anas2003@inbox.ru>
2022-03-16 16:12:42 +0300

    Fixed mistake in checkout because of which customer could not be seen by backend. Customer-user relation is fixed

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -43,6 +45,15 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+    @receiver(post_save, sender=User)
+    def create_customer_from_user(sender, instance, created, **kwargs):
+        if created:
+            Customer.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_customer(sender, instance, **kwargs):
+        instance.customer.save()
 
 
 class Order(models.Model):
